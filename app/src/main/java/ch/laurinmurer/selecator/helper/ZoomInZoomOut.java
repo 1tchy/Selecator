93e690b9523136f575c37c3313251fa586c222c5
Itchy <itchy@laurinmurer.ch>
2022-09-16 15:55:03 +0200

    Refactored ZoomInZoomOut

--- app/src/main/java/ch/laurinmurer/selecator/helper/ZoomInZoomOut.java
+++ app/src/main/java/ch/laurinmurer/selecator/helper/ZoomInZoomOut.java
@@ -9,84 +9,76 @@ import android.widget.ImageView;
 
 public class ZoomInZoomOut implements View.OnTouchListener {
 	private static final String TAG = "Touch";
-	@SuppressWarnings("unused")
-	private static final float MIN_ZOOM = 1f, MAX_bZOOM = 1f;
 
 	// These matrices will be used to scale points of the image
-	Matrix matrix = new Matrix();
-	Matrix savedMatrix = new Matrix();
+	private final Matrix newMatrix = new Matrix();
+	private final Matrix matrixBeforeAction = new Matrix();
 
-	// The 3 states (events) which the user is trying to perform
-	static final int NONE = 0;
-	static final int DRAG = 1;
-	static final int ZOOM = 2;
-	int mode = NONE;
+	private CurrentAction mode = CurrentAction.NONE;
 
 	// these PointF objects are used to record the point(s) the user is touching
-	PointF start = new PointF();
-	PointF mid = new PointF();
-	float oldDist = 1f;
+	private final PointF firstTouchPoint = new PointF();
+	private final PointF middleBetweenInitialTouchPoints = new PointF();
+	private float distanceBetweenInitialTouchPoints = 1f;
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		ImageView view = (ImageView) v;
 		view.setScaleType(ImageView.ScaleType.MATRIX);
-		float scale;
 
 		dumpEvent(event);
 		// Handle touch events here...
 
 		switch (event.getAction() & MotionEvent.ACTION_MASK) {
 			case MotionEvent.ACTION_DOWN:   // first finger down only
-				matrix.set(view.getImageMatrix());
-				savedMatrix.set(matrix);
-				start.set(event.getX(), event.getY());
-				Log.d(TAG, "mode=DRAG"); // write to LogCat
-				mode = DRAG;
+				newMatrix.set(view.getImageMatrix());
+				matrixBeforeAction.set(newMatrix);
+				firstTouchPoint.set(event.getX(), event.getY());
+				Log.d(TAG, "mode=DRAG");
+				mode = CurrentAction.DRAG;
 				break;
 
-			case MotionEvent.ACTION_UP: // first finger lifted
-
 			case MotionEvent.ACTION_POINTER_UP: // second finger lifted
-
-				mode = NONE;
+			case MotionEvent.ACTION_UP: // first finger lifted
+				mode = CurrentAction.NONE;
 				Log.d(TAG, "mode=NONE");
 				break;
 
 			case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-
-				oldDist = spacing(event);
-				Log.d(TAG, "oldDist=" + oldDist);
-				if (oldDist > 5f) {
-					savedMatrix.set(matrix);
-					midPoint(mid, event);
-					mode = ZOOM;
+				distanceBetweenInitialTouchPoints = spacing(event);
+				Log.d(TAG, "distanceBetweenInitialTouchPoints=" + distanceBetweenInitialTouchPoints);
+				if (distanceBetweenInitialTouchPoints > 5f) {
+					matrixBeforeAction.set(newMatrix);
+					middleBetweenInitialTouchPoints.set(
+							(event.getX(0) + event.getX(1)) / 2,
+							(event.getY(0) + event.getY(1)) / 2
+					);
+					mode = CurrentAction.ZOOM;
 					Log.d(TAG, "mode=ZOOM");
 				}
 				break;
 
 			case MotionEvent.ACTION_MOVE:
-
-				if (mode == DRAG) {
-					matrix.set(savedMatrix);
-					matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
-				} else if (mode == ZOOM) {
+				if (mode == CurrentAction.DRAG) {
+					newMatrix.set(matrixBeforeAction);
+					newMatrix.postTranslate(event.getX() - firstTouchPoint.x, event.getY() - firstTouchPoint.y); // create the transformation in the matrix  of points
+				} else if (mode == CurrentAction.ZOOM) {
 					// pinch zooming
 					float newDist = spacing(event);
 					Log.d(TAG, "newDist=" + newDist);
 					if (newDist > 5f) {
-						matrix.set(savedMatrix);
-						scale = newDist / oldDist; // setting the scaling of the
+						newMatrix.set(matrixBeforeAction);
+						float scale = newDist / distanceBetweenInitialTouchPoints; // setting the scaling of the
 						// matrix...if scale > 1 means
 						// zoom in...if scale < 1 means
 						// zoom out
-						matrix.postScale(scale, scale, mid.x, mid.y);
+						newMatrix.postScale(scale, scale, middleBetweenInitialTouchPoints.x, middleBetweenInitialTouchPoints.y);
 					}
 				}
 				break;
 		}
 
-		view.setImageMatrix(matrix); // display the transformation on screen
+		view.setImageMatrix(newMatrix); // display the transformation on screen
 
 		return true; // indicate event was handled
 	}
@@ -109,24 +101,19 @@ public class ZoomInZoomOut implements View.OnTouchListener {
 	 * Description: calculates the midpoint between the two fingers
 	 * ------------------------------------------------------------
 	 */
-	private void midPoint(PointF point, MotionEvent event) {
-		float x = event.getX(0) + event.getX(1);
-		float y = event.getY(0) + event.getY(1);
-		point.set(x / 2, y / 2);
-	}
 
 	/**
 	 * Show an event in the LogCat view, for debugging
 	 */
 	private void dumpEvent(MotionEvent event) {
-		String names[] = {"DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE", "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?"};
+		String[] names = {"DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE", "POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?"};
 		StringBuilder sb = new StringBuilder();
 		int action = event.getAction();
 		int actionCode = action & MotionEvent.ACTION_MASK;
 		sb.append("event ACTION_").append(names[actionCode]);
 
 		if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
-			sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+			sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_INDEX_SHIFT);
 			sb.append(")");
 		}
 
@@ -143,4 +130,7 @@ public class ZoomInZoomOut implements View.OnTouchListener {
 		sb.append("]");
 		Log.d("Touch Events ---------", sb.toString());
 	}
+
+	// The 3 states (events) which the user is trying to perform
+	private enum CurrentAction {NONE, DRAG, ZOOM}
 }
